57ed1a1fb12020f2188fae09d727070f56357984
dcoleslaw86 <cole.desrosiers86@gmail.com>
2020-07-09 16:13:56 -0500

    another heroku test

--- public/assets/js/script.js
+++ public/assets/js/script.js
@@ -1,18 +1,36 @@
-  $(function() {
-    $(".create-form").on("submit", function(event) {
-        event.preventDefault();
-        var newBurger = {
-            burger_name: $("#newburger").val().trim(),
-            devoured: 0
-        };
+  // $(function() {
+  //   $(".create-form").on("submit", function(event) {
+  //       event.preventDefault();
+  //       var newBurger = {
+  //           burger_name: $("#newburger").val().trim(),
+  //           devoured: 0
+  //       };
 
-        $.ajax("/api/burgers", {
-        type: "POST",
-        data: newBurger
-        }).then(
-        function() {
-            location.reload();
-        }
-        );
+  //       $.ajax("/api/burgers", {
+  //       type: "POST",
+  //       data: newBurger
+  //       }).then(
+  //       function() {
+  //           location.reload();
+  //       }
+  //       );
+  //   });
+  // });
+
+
+
+  $(document).ready(function() {
+    
+    $(".devour-form").on("submit", function(event) {
+      event.preventDefault();
+  
+      var burger_id = $(this).children(".burger_id").val();
+      console.log(burger_id);
+      $.ajax({
+        method: "PUT",
+        url: "/burgers/" + burger_id
+      }).then(function(data) {
+        location.reload();
+      });
     });
   });
