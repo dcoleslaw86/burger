b416b6e4e16e97971f30bd1f4cd9b1bbaaae97ed
dcoleslaw86 <cole.desrosiers86@gmail.com>
2020-07-09 15:30:16 -0500

    adding files that have work done

---
+++ db/seeds.sql
@@ -0,0 +1,3 @@
+INSERT INTO burgers (burger_name, devoured) VALUES ('Fish Burger', FALSE);
+INSERT INTO burgers (burger_name, devoured) VALUES ('Hamburger', FALSE);
+INSERT INTO burgers (burger_name, devoured) VALUES ('Pizza Burger', FALSE);
