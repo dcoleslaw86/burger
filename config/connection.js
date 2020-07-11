0d251ad22c309f3015ad67a545e47171feb1aa1e
dcoleslaw86 <cole.desrosiers86@gmail.com>
2020-07-11 17:52:29 -0500

    took time to make major adjustments and a tutoiral for help

--- config/connection.js
+++ config/connection.js
@@ -1,17 +1,13 @@
-var mysql = require("mysql");
-
-var connection;
-
-if (process.env.JAWSDB_URL){
-    connection = mysql.createConnection(process.env.JAWSDB_URL);
+const mysql = require("mysql");
+if (process.env.JAWSDB_URL) {
+  connection = mysql.createConnection(process.env.JAWSDB_URL);
 } else {
-    connection = mysql.createConnection({
-        host: "localhost",
-        user: "root",
-        password: "password",
-        database: "burgers_db"
-    });
-};
-
+  connection = mysql.createConnection({
+    host: "localhost",
+    user: "root",
+    password: "password",
+    database: "burgers_db"
+  });
+}
 connection.connect();
 module.exports = connection;
--- config/orm.js
+++ config/orm.js
@@ -1,56 +1,84 @@
-var connection = require("./connection.js");
-function printQuestionMarks(num) {
+var connection = require("../config/connection");
+
+function createQmarks(num) {
   var arr = [];
   for (var i = 0; i < num; i++) {
     arr.push("?");
   }
   return arr.toString();
 }
-function objToSql(ob) {
+
+function translateSql(ob) {
   var arr = [];
   for (var key in ob) {
-    arr.push(key + "=" + ob[key]);
+    var value = ob[key];
+    if (Object.hasOwnProperty.call(ob, key)) {
+      if (typeof value === "string" && value.indexOf(" ") >= 0) {
+        value = "'" + value + "'";
+      }
+      arr.push(key + "=" + value);
+    }
   }
   return arr.toString();
 }
+
 var orm = {
-  all: function(tableInput, cb) {
-    var queryString = "SELECT * FROM " + tableInput + ";";
-    connection.query(queryString, function(err, result) {
+  selectAll: function(table, cb) {
+    var dbQuery = "SELECT * FROM " + table + ";";
+
+    connection.query(dbQuery, function(err, res) {
       if (err) {
         throw err;
       }
-      cb(result);
+      cb(res);
     });
   },
-  create: function(table, cols, vals, cb) {
-    var queryString = "INSERT INTO " + table;
-    queryString += " (";
-    queryString += cols.toString();
-    queryString += ") ";
-    queryString += "VALUES (";
-    queryString += printQuestionMarks(vals.length);
-    queryString += ") ";
-    console.log(queryString);
-    connection.query(queryString, vals, function(err, result) {
+  insertOne: function(table, cols, vals, cb) {
+    var dbQuery =
+      "INSERT INTO " +
+      table +
+      " (" +
+      cols.toString() +
+      ") " +
+      "VALUES (" +
+      createQmarks(vals.length) +
+      ") ";
+
+    console.log(dbQuery);
+    connection.query(dbQuery, vals, function(err, res) {
       if (err) {
         throw err;
       }
-      cb(result);
+      cb(res);
     });
   },
-  update: function(table, objColVals, condition, cb) {
-    var queryString = "UPDATE " + table;
-    queryString += " SET ";
-    queryString += objToSql(objColVals);
-    queryString += " WHERE ";
-    queryString += condition;
-    console.log(queryString);
-    connection.query(queryString, function(err, result) {
+  updateOne: function(table, objColVals, condition, cb) {
+    var dbQuery =
+      "UPDATE " +
+      table +
+      " SET " +
+      translateSql(objColVals) +
+      " WHERE " +
+      condition;
+
+    console.log(dbQuery);
+
+    connection.query(dbQuery, function(err, res) {
       if (err) {
         throw err;
       }
-      cb(result);
+      cb(res);
+    });
+  },
+  deleteOne: function(table, condition, cb) {
+    var dbQuery = "DELETE FROM " + table + " WHERE " + condition;
+    console.log(dbQuery);
+
+    connection.query(dbQuery, function(err, res) {
+      if (err) {
+        throw err;
+      }
+      cb(res);
     });
   }
 };
